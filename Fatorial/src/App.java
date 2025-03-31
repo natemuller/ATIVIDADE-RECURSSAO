e8f30bc3e06f03032f9cf02328a1292eb27cb682
natan gostosao <natan.muller@edu.pucrs.br>
2025-03-31 19:53:26 -0300

    adicionado scanner para facilitar a entrada dos valores

--- Fatorial/src/App.java
+++ Fatorial/src/App.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class App {
 
     public static int Fatorial (int num) {
@@ -7,8 +8,10 @@ public class App {
             return num * Fatorial (num - 1);
         }
     }
-    public static void main(String[] args) throws Exception {
-        int num = Integer.parseInt(args[0]);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
         System.out.printf("%d\n", Fatorial(num));
+        scanner.close();
     }
 }
